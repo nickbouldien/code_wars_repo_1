3d6b11de0cc3e8817722dc7beb51be0c7f033404
nick bouldien <nick.bouldien@gmail.com>
2017-04-04 23:31:52 -0700

    first commit of code wars challenges

---
+++ code_wars_megafile_1.js
@@ -0,0 +1,78 @@
+
+
+
+
+// The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. A "Avengers" ticket costs 25 dollars.
+//
+// Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+//
+// Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
+//
+// Return YES, if Vasya can sell a ticket to each person and give the change. Otherwise return NO.
+
+// Test.assertEquals(tickets([25, 25, 50, 50]), "YES");
+// Test.assertEquals(tickets([25, 100]), "NO");
+//
+
+
+function tickets(peopleInLine){
+
+  var total = 0
+
+  peopleInLine.forEach(function(element) {
+    total += element;
+
+    
+  })
+
+  peopleInLine.forEach(function(element) {
+    total -= element;
+
+    if (total < 0) {
+      return "NO";
+    }
+  })
+
+  var fans = peopleInLine.length;
+  return total;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//

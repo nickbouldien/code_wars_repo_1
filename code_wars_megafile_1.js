15bd93a43424702d27152f2ceddc398407839819
nick bouldien <nick.bouldien@gmail.com>
2017-04-30 12:10:10 -0700

    complete 2 new katas

--- code_wars_megafile_1.js
+++ code_wars_megafile_1.js
@@ -1,7 +1,3 @@
-
-
-
-
 // The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. A "Avengers" ticket costs 25 dollars.
 //
 // Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
@@ -15,61 +11,62 @@
 //
 
 
-function tickets(peopleInLine){
+function tickets(peopleInLine) {
 
-  var total = 0
+    var total = 0
 
-  peopleInLine.forEach(function(element) {
-    total += element;
+    peopleInLine.forEach(function(element) {
+        total += element;
 
-    
-  })
 
-  peopleInLine.forEach(function(element) {
-    total -= element;
+    })
 
-    if (total < 0) {
-      return "NO";
-    }
-  })
+    peopleInLine.forEach(function(element) {
+        total -= element;
 
-  var fans = peopleInLine.length;
-  return total;
+        if (total < 0) {
+            return "NO";
+        }
+    })
+
+    var fans = peopleInLine.length;
+    return total;
 }
 
 
+function isTriangle(a, b, c) {
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        return true
+    } else {
+        return false
+    }
+}
 
+//
+// var isSquare = function(n){
+//
+//   if (Math.sqrt(n) == n) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function GetSum(a, b) {
+    let sum = 0;
+    if (a == b) {
+        return a
+    } else if (a > b) {
+        for (var i = b; i <= a; i++) {
+            sum += i;
+        }
+    } else {
+        for (var i = a; i <= b; i++) {
+            sum += i;
+        }
+    }
+    return sum
+}
 
 
 
